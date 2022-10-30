ec77bffe8d5a68cfccf592a1f0705b8a95140004
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2022-10-30 18:15:22 -0300

    add cors policy

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,18 @@ namespace car_rentals_project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(
+                options => {
+                    options.AddPolicy
+                    ("CorsPolicy", builder => builder.
+                        AllowAnyOrigin().
+                        AllowAnyHeader().
+                        AllowAnyMethod()
+                    );
+                }
+            );
+            // app.UseCors("CorsPolicy");
+            
             services.AddDbContext<DataContext>
             (
                 options => options.UseSqlite("DataSource=cars.db;Cache=shared")
